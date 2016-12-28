475516f8f85230a6d68229cfa511c033ab0bb6dd
Alexandre Bintz <alexandre@bintz.io>
2016-12-28 14:43:32 +0100

    tests: remove non empty directory

--- test/index.js
+++ test/index.js
@@ -9,45 +9,92 @@ fsSandbox.setRoot(__dirname)
 let rimraf = require('./../src/index')
 
 describe('rimraf', function () {
-  it('callback', function (done) {
-    // ## Setup
-    let sandbox = fsSandbox.newSync()
-    let file = sandbox.touchpSync('foo/bar')
-    // ## TEST
-    rimraf(file.fullpath, (err) => {
+  describe('file', function () {
+    it('callback', function (done) {
+      // ## Setup
+      let sandbox = fsSandbox.newSync()
+      let file = sandbox.touchpSync('foo/bar')
+      // ## TEST
+      rimraf(file.fullpath, (err) => {
+        // ## Assert
+        expect(err).to.be.null
+        expect(file.fullpath).to.not.be.a.path()
+        // ## Teardown
+        fsSandbox.rmSync()
+        // ## End
+        done()
+      })
+    })
+    it('promise', function (done) {
+      // ## Setup
+      let sandbox = fsSandbox.newSync()
+      let file = sandbox.touchpSync('foo/bar')
+      // ## TEST
+      rimraf(file.fullpath).then(() => {
+        // ## Assert
+        expect(file.fullpath).to.not.be.a.path()
+        // ## Teardown
+        fsSandbox.rmSync()
+        // ## End
+      }).then(() => done()).catch(done)
+    })
+    it('sync', function () {
+      // ## Setup
+      let sandbox = fsSandbox.newSync()
+      let file = sandbox.touchpSync('foo/bar')
+      // ## TEST
+      rimraf.sync(file.fullpath)
       // ## Assert
-      expect(err).to.be.null
       expect(file.fullpath).to.not.be.a.path()
       // ## Teardown
       fsSandbox.rmSync()
       // ## End
-      done()
     })
   })
-  it('promise', function (done) {
-    // ## Setup
-    let sandbox = fsSandbox.newSync()
-    let file = sandbox.touchpSync('foo/bar')
-    // ## TEST
-    rimraf(file.fullpath).then(() => {
+  describe('dir', function () {
+    it('callback', function (done) {
+      // ## Setup
+      let sandbox = fsSandbox.newSync()
+      let dir = sandbox.mkdirpSync('foo/bar')
+      sandbox.touchpSync('foo/bar/foo.txt')
+      // ## TEST
+      rimraf(dir.fullpath, (err) => {
+        // ## Assert
+        expect(err).to.be.null
+        expect(dir.fullpath).to.not.be.a.path()
+        // ## Teardown
+        fsSandbox.rmSync()
+        // ## End
+        done()
+      })
+    })
+    it('promise', function (done) {
+      // ## Setup
+      let sandbox = fsSandbox.newSync()
+      let dir = sandbox.mkdirpSync('foo/bar')
+      sandbox.touchpSync('foo/bar/foo.txt')
+      // ## TEST
+      rimraf(dir.fullpath).then(() => {
+        // ## Assert
+        expect(dir.fullpath).to.not.be.a.path()
+        // ## Teardown
+        fsSandbox.rmSync()
+        // ## End
+      }).then(() => done()).catch(done)
+    })
+    it('sync', function () {
+      // ## Setup
+      let sandbox = fsSandbox.newSync()
+      let dir = sandbox.mkdirpSync('foo/bar')
+      sandbox.touchpSync('foo/bar/foo.txt')
+      // ## TEST
+      rimraf.sync(dir.fullpath)
       // ## Assert
-      expect(file.fullpath).to.not.be.a.path()
+      expect(dir.fullpath).to.not.be.a.path()
       // ## Teardown
       fsSandbox.rmSync()
       // ## End
-    }).then(() => done()).catch(done)
-  })
-  it('sync', function () {
-    // ## Setup
-    let sandbox = fsSandbox.newSync()
-    let file = sandbox.touchpSync('foo/bar')
-    // ## TEST
-    rimraf.sync(file.fullpath)
-    // ## Assert
-    expect(file.fullpath).to.not.be.a.path()
-    // ## Teardown
-    fsSandbox.rmSync()
-    // ## End
+    })
   })
   after(function () {
     fsSandbox.rmSync()
