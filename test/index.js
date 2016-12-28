b28aeb369fbf7f85995dcb359d3de43848086704
Alexandre Bintz <alexandre@bintz.io>
2016-12-28 14:41:00 +0100

    tests: reorder tests

--- test/index.js
+++ test/index.js
@@ -9,19 +9,6 @@ fsSandbox.setRoot(__dirname)
 let rimraf = require('./../src/index')
 
 describe('rimraf', function () {
-  it('promise', function (done) {
-    // ## Setup
-    let sandbox = fsSandbox.newSync()
-    let file = sandbox.touchpSync('foo/bar')
-    // ## TEST
-    rimraf(file.fullpath).then(() => {
-      // ## Assert
-      expect(file.fullpath).to.not.be.a.path()
-      // ## Teardown
-      fsSandbox.rmSync()
-      // ## End
-    }).then(() => done()).catch(done)
-  })
   it('callback', function (done) {
     // ## Setup
     let sandbox = fsSandbox.newSync()
@@ -36,6 +23,19 @@ describe('rimraf', function () {
       // ## End
       done()
     })
+  })
+  it('promise', function (done) {
+    // ## Setup
+    let sandbox = fsSandbox.newSync()
+    let file = sandbox.touchpSync('foo/bar')
+    // ## TEST
+    rimraf(file.fullpath).then(() => {
+      // ## Assert
+      expect(file.fullpath).to.not.be.a.path()
+      // ## Teardown
+      fsSandbox.rmSync()
+      // ## End
+    }).then(() => done()).catch(done)
   })
   it('sync', function () {
     // ## Setup
